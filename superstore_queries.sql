0b1d1739841a22e38a9eec4865cef43f385437c2
kabulsoud <114303604+kabulsoud@users.noreply.github.com>
2025-04-16 21:40:41 -0400

    Update superstore_queries.sql

--- superstore_queries.sql
+++ superstore_queries.sql
@@ -11,3 +11,11 @@ SELECT region, SUM (sales) AS total_sales
 FROM superstore 
 GROUP BY region 
 ORDER BY total_sales DESC
+
+
+-- Month to month total sales comparison
+
+SELECT EXTRACT(MONTH FROM order_date) as month, EXTRACT(YEAR FROM order_date) AS year, SUM(sales) AS total_sales 
+FROM superstore
+GROUP BY month, year
+ORDER BY year ASC , month ASC
