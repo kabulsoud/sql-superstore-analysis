5f0e2e6e359de3367dbe747d2909f18c36c1ffd5
kabulsoud <114303604+kabulsoud@users.noreply.github.com>
2025-04-16 21:12:52 -0400

    Update superstore_queries.sql

--- superstore_queries.sql
+++ superstore_queries.sql
@@ -4,3 +4,10 @@ FROM superstore
 GROUP BY product_name 
 ORDER by total_profit DESC
 LIMIT 10
+
+
+-- regions  generating the highest total sales
+SELECT region, SUM (sales) AS total_sales
+FROM superstore 
+GROUP BY region 
+ORDER BY total_sales DESC
